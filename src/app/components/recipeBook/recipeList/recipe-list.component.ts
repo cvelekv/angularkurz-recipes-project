0bf67ede3139e6b180452e94249292c80aa2d9f7
nicvetkovi <nikola.cvetkovic@eng.it>
2018-07-06 15:40:32 +0200

    building components

--- src/app/components/recipeBook/recipeList/recipe-list.component.ts
+++ src/app/components/recipeBook/recipeList/recipe-list.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from './../recipe.model';
 
 @Component({
   selector: "app-recipe-list",
   templateUrl: "./recipe-list.component.html"
 })
-export class RecipeList {
+export class RecipeList implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      "A Test Recipe",
+      "This is simply a test",
+      "https://s3.amazonaws.com/finecooking.s3.tauntonclud.com/app/uploads/2017/04/18180350/051SIP112-grilled-mustard-rosemary-chicken-recipe-alt-main.jpg"
+    ),
+    new Recipe(
+      "A Test Recipe",
+      "This is simply a test",
+      "https://s3.amazonaws.com/finecooking.s3.tauntonclud.com/app/uploads/2017/04/18180350/051SIP112-grilled-mustard-rosemary-chicken-recipe-alt-main.jpg"
+    )
+  ];
   constructor() {}
+  ngOnInit(): void {}
 }
