936f2a7131469145308a640f647dd21687801ffb
nicvetkovi <nikola.cvetkovic@eng.it>
2018-07-06 11:55:54 +0200

    components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { RecipeDetail } from './components/recipeBook/recipeDetail/recipe-detail.component';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RecipeItem } from './components/recipeBook/recipeItem/recipe-item.component';
+import { RecipeList } from './components/recipeBook/recipeList/recipe-list.component';
+import { ShoppingListComponent } from './components/shoppingList/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './components/shoppingList/shopping-list-edit/shopping-list-edit.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, RecipeList, RecipeItem, RecipeDetail, ShoppingListComponent, ShoppingListEditComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
